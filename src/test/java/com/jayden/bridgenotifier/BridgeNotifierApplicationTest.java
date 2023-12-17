5469ef767733833a34e9a606933b9bf0eb05bf0b
Jayden <kikiki0611@naver.com>
2023-12-18 02:40:32 +0900

    docs: readme 및 테스트 코드에 user story 추가

---
+++ src/test/java/com/jayden/bridgenotifier/BridgeNotifierApplicationTest.java
@@ -0,0 +1,59 @@
+package com.jayden.bridgenotifier;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BridgeNotifierApplicationTest {
+    @Test
+    @DisplayName("사용자는, 구독할 RSS 피드를 등록할 수 있다.")
+    void case1() {
+        fail();
+    }
+
+    @Test
+    @DisplayName("사용자는, 알림받을 시간을 설정할 수 있다.")
+    void case2() {
+        fail();
+    }
+
+    @Test
+    @DisplayName("사용자는, 알림받을 메일을 설정할 수 있다.")
+    void case2_1() {
+        fail();
+    }
+
+
+    @Test
+    @DisplayName("피드 시스템은, N초 간격으로 읽어야될 피드를 확인한다.")
+    void case3() {
+        fail();
+    }
+
+    @Test
+    @DisplayName("피드 시스템은, 업데이트 된 피드가 있으면, 알림 시스템에 알림을 예약한다.")
+    void case4() {
+        fail();
+    }
+
+    @Test
+    @DisplayName("피드는, 피드를 요약할 수 있다.")
+    void case6() {
+        fail();
+    }
+
+    @Test
+    @DisplayName("피드는, 피드를 번역할 수 있다.")
+    void case7() {
+        fail();
+    }
+
+    @Test
+    @DisplayName("알림 시스템은, 예약된 알림을 사용자에게 전달한다.")
+    void case5() {
+        fail();
+    }
+}
